4d81e007f971fbc38ef7d03e711dc61083adb207
eyal sharabi <eyal.shara@gmail.com>
2026-09-10 19:13:20 +0300

    added meepmeep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP/ErmiasTeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP/ErmiasTeleOP.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Libraries.JeruLib.JeruOpMode;
 import org.firstinspires.ftc.teamcode.Libraries.JeruLib.JeruRobot;
 import org.firstinspires.ftc.teamcode.Libraries.JeruLib.Utils.OpModeType;
+import org.firstinspires.ftc.teamcode.SubSystems.ermias_drive;
 
 @TeleOp
 public class ErmiasTeleOP extends JeruOpMode {
@@ -14,6 +15,7 @@ public class ErmiasTeleOP extends JeruOpMode {
         robotInstance = JeruRobot.getInstance();
         robotInstance.initJeruRobot()
                 .opModeType(OpModeType.EXPERIMENTING_NO_EXPANSION)
+                .WithoutDriveTrain()
                 .build(this);
 //
 //        robotInstance.gamepadEx1.getGamepadButton(GamepadKeys.Button.A).toggleWhenPressed(
@@ -24,6 +26,9 @@ public class ErmiasTeleOP extends JeruOpMode {
 //        robotInstance.gamepadEx1.getGamepadButton(GamepadKeys.Button.Y).whenPressed(
 //                clawSubsystem.getInstance().OpenServo()
 //        );
+        ermias_drive.getInstance().setDefaultCommand(
+                ermias_drive.getInstance().Move()
+        );
     }
 
 
